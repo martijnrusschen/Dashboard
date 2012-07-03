cffdc2a28bed1a9735f2bd4c2f229cbfe4507fef
Martijn Russchen <martijn@factlink.com>
2012-07-04 01:08:46 +0200

    Deleted example stuff and added own descriptions

--- widgets/nl.martijnrusschen/pingdom/widget.php
+++ widgets/nl.martijnrusschen/pingdom/widget.php
@@ -1,11 +1,11 @@
 <?php if (defined('BAS')) require dirname(__FILE__) . DS . 'config.php'; else die(); ?>
 <?php if (isset($config['nl.martijnrusschen']['pingdom']) && $config['nl.martijnrusschen']['pingdom']['enabled']) { ?>
     <!-- HTML / PHP -->
-    <li id="nl-example-example">
+    <li id="nl-martijnrusschen-pingdom">
         <div class="box">
             <!-- Widget content -->
         </div>
-        <label><?php echo $config['nl.example']['example']['label']; ?></label>
+        <label><?php echo $config['nl.martijnrusschen']['pingdom']['label']; ?></label>
     </li>
     <!-- END -->
 
